326fe789f3742e8167d9c0dcca8b15ce26e3cf92
Agustin Lanusse <aglanuss@cbr15s3.42barcelona.com>
2023-07-24 12:02:48 +0200

    final commit

--- ex00/auxiliar_functions.c
+++ ex00/auxiliar_functions.c
@@ -6,167 +6,82 @@
 /*   By: mpovill- <mpovill-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 11:02:26 by mpovill-          #+#    #+#             */
-/*   Updated: 2023/07/22 18:04:05 by mpovill-         ###   ########.fr       */
+/*   Updated: 2023/07/23 23:13:24 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 
-void	ft_putstr(char *str);
-int	ft_strlen(char *str);
+void			ft_putstr(char *str);
+int				ft_strlen(char *str);
+int				search_in_file(char *str, char *name);
+int				recursive_create_array(int nbr, char *str, int index);
+char			*copy_str(char *str_to_copy, int num);
 
-
-/*
- * This will never get an empty string. It should always be correct
- */
-unsigned int	get_digits(char *str)
-{
-	unsigned int	mult;
-
-	mult = 1;
-	str++;
-	while (*str >= '0' && *str <= '9')
-	{
-		mult *= 10;
-		str++;
-	}
-	return (mult);
-}
-
-/*
- * Called at *ft_nbr_to_char. Used to operate
- */
-int	recursive_create_array(int nbr, char *str, int index)
-{
-	if ((nbr / 10) < 1)
-		*str = nbr + '0';
-	else
-	{
-		index = recursive_create_array(nbr / 10, str, index);
-		*(str + index) = (nbr % 10) + '0';
-	}
-	index++;
-	return (index);
-}
-
-/*
- * Converts an int to an array of char
- */
-char	*ft_nbr_to_char(int nb, int size)
+char	*copy_str_and_add(char *str_to_copy)
 {
 	char	*str;
-	int		index;
 
-	str = (char *)malloc(sizeof(char) * size);
-	//check null??
-	index = 0;
-	if (nb < 0)
-	{
-		*str = '-';
-		index++;
-	}
-	index = recursive_create_array(nb, str, index);
-	*(str + index) = '\0';
+	str = (char *)malloc(sizeof(char) * 3);
+	str[0] = str_to_copy[0];
+	str[1] = '0';
+	str[2] = '\0';
 	return (str);
 }
 
-/*
- * This manages the string we pass as an argument and gets
- * all the numbers to search and display at the dictionary
- */
-void	ft_manage_str(char *str)
+int	search_all(unsigned int digits, char *str, char *name)
 {
-	int				aux;
-	int				length;
-	unsigned int	digits;
-	char			*str_aux;
+	int	aux;
 
-	length = ft_strlen(str);
-	digits = get_digits(str);
-	while (digits > 0)
+	aux = 1;
+	if (*str != '0')
 	{
-		if (digits < 100)
+		if (digits < 10)
+			aux = search_in_file(copy_str(str, 1), name);
+		else if (digits < 100)
 		{
-			if (*str <= '2' && *str > '0')
+			if (*str == '1')
 			{
-				//add num together
-				//str_aux = ft_nbr_to_char(digits, length);
-				ft_putstr(str);
-				digits = 0;
+				aux = search_in_file(copy_str(str, 2), name);
+				if (aux != 0)
+					aux = -1;
 			}
-		}
-		else if (digits == 1 && *str > '0')
-		{
-			write(1, str, 1);
-			write(1, "\n", 1);
+			else
+				aux = search_in_file(copy_str_and_add(str), name);
 		}
 		else
 		{
-			aux = *str;
-			if (aux != '0')
-			{
-				write(1, &aux, 1);
-				write(1, "\n", 1);
-				str_aux = ft_nbr_to_char(digits, length);
-				ft_putstr(str_aux);
-			}
+			aux = search_in_file(copy_str(str, 1), name);
+			aux = search_in_file("100", name);
 		}
-		digits /= 10;
-		str++;
 	}
+	return (aux);
 }
 
-void	search_two_digits(char first, char second)
+int	search_number(unsigned int digits, char *str, char *name)
 {
-	//write(1, &first, 1);
-	//write(1, &second, 1);
-}
+	int	*i;
+	int	num;
+	int	aux;
 
-int	search_number(unsigned int digits, char *str)
-{
-	char *str_aux;
-	int	i;
-
-	i = 0;
+	num = 0;
+	i = &num;
 	while (digits > 0)
 	{
-		if (*str != '0')
+		aux = search_all(digits, str, name);
+		if (aux == 0)
+			return (0);
+		if (aux == -1)
 		{
-			if (digits < 10)
-			{
-				write(1, str, 1);
-				write(1, "\n", 1);
-			}
-			else if (digits < 100)
-			{
-				if (*str == '1')
-				{
-					write(1, str, 2);
-					write(1, "\n", 1);
-					digits = 1;
-					i++;
-				}
-				else
-				{
-					write(1, str, 1);
-					write(1, "0", 1);
-					write(1, "\n", 1);
-				}
-			}
-			else
-			{
-				write(1, str, 1);
-				write(1, "\n", 1);
-				write(1, "100", 3);
-				write(1, "\n", 1);
-			}
+			*i += 1;
+			digits = 0;
 		}
 		str++;
-		i++;
+		*i += 1;
 		digits /= 10;
 	}
-	return (i);
+	return (num);
 }
 
 unsigned int	update_digits(unsigned int digits, int i)
@@ -182,37 +97,13 @@ unsigned int	update_digits(unsigned int digits, int i)
 	return (digits);
 }
 
-void	ft_lookup_numbers_dictionary(char *str)
+int	check_range_not_zero(int number, char *str)
 {
-	unsigned int	digits;
-	int	i;
-	int	length;
-
-	length = ft_strlen(str);
-	digits = get_digits(str);
-	while (digits > 0)
-	{
-		if (digits >= 1000000)
-		{
-			i = search_number(digits / 1000000, str);
-			str = str + i;
-			digits = update_digits(digits, i);
-			write(1, "1000000", 7);
-			write(1, "\n", 1);
-		}
-		else if (digits >= 1000)
-		{
-			i = search_number(digits / 1000, str);
-			str = str + i;
-			digits = update_digits(digits, i);
-			write(1, "1000", 4);
-			write(1, "\n", 1);
-		}
-		else
-		{
-			i = search_number(digits, str);
-			str = str + i;
-			digits = update_digits(digits, i);
-		}
-	}
+	if (number == 100 && str[0] == '0' && str[1] == '0' && str[2] == '0')
+		return (0);
+	else if (number == 10 && str[0] == '0' && str[1] == '0')
+		return (0);
+	else if (number == 1 && str[0] == '0')
+		return (0);
+	return (1);
 }
